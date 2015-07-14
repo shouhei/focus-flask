3e73adc7a9bd2113df83320e345d2567bf27987f
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-07-14 18:06:02 +0900

    fix create tables

--- migrate/versions/001_add_account_table.py
+++ migrate/versions/001_add_account_table.py
@@ -3,20 +3,57 @@ from migrate import *
 import datetime
 
 meta = MetaData()
-timer_start = Table(
-    'timer_start', meta,
+
+organizations = Table(
+    'organizations', meta,
     Column('id', Integer, primary_key=True),
-    Column('user_id', Integer, nullable=False),
-    Column('timer_key', Integer, nullable=False),
+    Column('name', String(255), nullable=False),
     Column('created_at', DateTime, server_default=func.now(), nullable=False),
-    Column('modified_at', DateTime, server_default=func.now(), onupdate=func.now(),nullable=False),
+    Column('modified_at', DateTime, onupdate=func.now(),nullable=False),
+)
+
+users = Table(
+    'users', meta,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(255), nullable=False),
+    Column('password', String(255), nullable=False),
+    Column('organization_id', ForeignKey('organizations.id')),
+    Column('created_at', DateTime, server_default=func.now(), nullable=False),
+    Column('modified_at', DateTime, onupdate=func.now(),nullable=False),
+)
+
+spots = Table(
+    'spots', meta,
+    Column('id', Integer, primary_key=True),
+    Column('forsquare_id', String(255), nullable=False),
+    Column('name', Integer, nullable=False),
+    Column('created_at', DateTime, server_default=func.now(), nullable=False),
+    Column('modified_at', DateTime, onupdate=func.now(),nullable=False),
+)
+
+timers = Table(
+    'timers', meta,
+    Column('id', Integer, primary_key=True),
+    Column('user_id', ForeignKey('users.id')),
+    Column('spot_id', ForeignKey('spots.id')),
+    Column('start_at', DateTime, nullable=False),
+    Column('end_at', DateTime),
+    Column('result_time', Time),
+    Column('created_at', DateTime, server_default=func.now(), nullable=False),
+    Column('modified_at', DateTime, onupdate=func.now(),nullable=False),
 )
 
 def upgrade(migrate_engine):
     meta.bind = migrate_engine
-    timer_start.create()
+    organizations.create()
+    users.create()
+    spots.create()
+    timers.create()
 
 def downgrade(migrate_engine):
     meta.bind = migrate_engine
-    timer_start.drop()
+    timers.drop()
+    spots.drop()
+    users.drop()
+    organizations.drop()
 
