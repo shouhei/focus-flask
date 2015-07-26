72503f3c23994101e080fed9b43a0f3d80372eef
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-07-27 08:11:37 +0900

    change column define in spot

--- model/spot.py
+++ model/spot.py
@@ -1,3 +1,18 @@
 from model.model import AppModel
+from sqlalchemy import func
+from sqlalchemy import Column
+from sqlalchemy.types import UserDefinedType
+
+class Geometry(UserDefinedType):
+    def get_col_spec(self):
+        return "GEOMETRY"
+
+    def bind_expression(self, bindvalue):
+        return func.GeomFromText(bindvalue, type_=self)
+
+    def column_expression(self, col):
+        return func.GeomFromText(col, type_=self)
+
 class Spot(AppModel):
     __tablename__ = 'spots'
+    latlng = Column('latlng', Geometry)
