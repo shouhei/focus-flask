95fe018dd8c619ee5106940784978ce2206ae4d8
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-07-26 18:47:23 +0900

    fix timerstart

--- controller/timerstart.py
+++ controller/timerstart.py
@@ -4,16 +4,22 @@ from model.timer import Timer
 
 
 class TimerStartView(FlaskView):
-    def index(self):
-        return jsonify(status=200, message='ok', request='timer_start', response='')
 
     def post(self):
+        checked_request = self.__check_request(request.form)
         with Timer.transaction():
-            timer = Timer(user_id=request.form["user_id"],
-                          spot_id=request.form["spot_id"],
-                          start_at=request.form["start_at"]
+            timer = Timer(user_id=checked_request["user_id"],
+                          spot_id=checked_request["spot_id"],
+                          start_at=checked_request["start_at"]
             )
             timer.insert()
         return jsonify(status=200, message='ok', request=request.form,
                        response={'timer_id':timer.id}
                )
+
+    def __check_request(post):
+        if not 'spot_id' in post:
+            abort(400)
+        if not 'start_at' in post:
+            abort(400)
+        return post
