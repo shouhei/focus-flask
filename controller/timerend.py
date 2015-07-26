a2a3cbf6a499d73c15e749e8c7fb5298fe2d7027
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-07-26 16:42:13 +0900

    change result_time time to string

--- controller/timerend.py
+++ controller/timerend.py
@@ -11,8 +11,12 @@ class TimerEndView(FlaskView):
     def post(self):
         with Timer.transaction():
             timer = Timer.find(request.form['id'])
+            res = datetime.strptime(request.form['end_at'],'%Y-%m-%d %H:%M:%S') - timer.start_at
+            [tmp_minutes, seconds] = divmod(res.total_seconds(), 60)
+            [hours, minutes] = divmod(tmp_minutes,60)
+            result_time = '%03d:%02d:%02d' % (hours, minutes, seconds)
             timer.update(
                          end_at=request.form['end_at'],
-                         result_time=(datetime.strptime(request.form['end_at'],'%Y-%m-%d %H:%M:%S') - timer.start_at)
+                         result_time=result_time
             )
         return jsonify(status=200, message='ok', request=request.form, response="")
