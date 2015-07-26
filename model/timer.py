980a8188e0393aa78db9b5d3a9460e18379c112e
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-07-26 16:41:50 +0900

    add relationship to timer

--- model/timer.py
+++ model/timer.py
@@ -1,3 +1,9 @@
 from model.model import AppModel
+from model.spot import Spot
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relation
+
 class Timer(AppModel):
     __tablename__ = 'timers'
+    spot = relation('Spot', order_by='Spot.id',
+                          uselist=False, backref='timer')
