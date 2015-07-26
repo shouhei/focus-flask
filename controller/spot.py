a64d7fdb6862af2e4cf1aa08d0d4458d4681fe78
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-07-26 17:47:30 +0900

    add timers users relationship

--- controller/spot.py
+++ controller/spot.py
@@ -24,7 +24,7 @@ class SpotView(FlaskView):
         session = Timer._get_session()
         res = []
         sum_label = func.sec_to_time(func.sum(func.time_to_sec(Timer.result_time))).label('sum')
-        timer = session.query(Timer,sum_label).filter(Timer.end_at!=None, Timer.spot_id==1).group_by(Timer.user_id).order_by(sum_label.desc()).all()
+        timer = session.query(Timer, sum_label).filter(Timer.end_at!=None, Timer.spot_id==1).group_by(Timer.user_id).order_by(sum_label.desc()).all()
         n = 0
         for row in timer:
             n += 1;
@@ -32,8 +32,8 @@ class SpotView(FlaskView):
                 {
                     'rank':n,
                     'user':{
-                        'id':row[0].user_id,
-                        'name':"test"
+                        'id':row[0].user.id,
+                        'name':row[0].user.name
                     },
                     'sum':str(row.sum)
                 }
