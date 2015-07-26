c0ab99aabc7e70e3f269cbe27a8d6d050ae441be
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-07-26 18:47:09 +0900

    fix timerend

--- controller/timerend.py
+++ controller/timerend.py
@@ -5,13 +5,14 @@ from model.timer import Timer
 from datetime import datetime
 
 class TimerEndView(FlaskView):
-    def index(self):
-        return jsonify(status=200, message='ok', request='hello', response='')
 
     def post(self):
+        checked_request = self.__check_request(request.form)
         with Timer.transaction():
-            timer = Timer.find(request.form['id'])
-            res = datetime.strptime(request.form['end_at'],'%Y-%m-%d %H:%M:%S') - timer.start_at
+            timer = Timer.find(checked_request['id'])
+            if not timer:
+                abort(404)
+            res = datetime.strptime(checked_request['end_at'],'%Y-%m-%d %H:%M:%S') - timer.start_at
             [tmp_minutes, seconds] = divmod(res.total_seconds(), 60)
             [hours, minutes] = divmod(tmp_minutes,60)
             result_time = '%02d:%02d:%02d' % (hours, minutes, seconds)
@@ -20,3 +21,10 @@ class TimerEndView(FlaskView):
                          result_time=result_time
             )
         return jsonify(status=200, message='ok', request=request.form, response="")
+
+    def __check_request(post):
+        if not 'id' in post:
+            abort(400)
+        if not 'checked_request' in post:
+            abort(400)
+        return post
